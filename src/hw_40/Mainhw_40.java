9f0bf46d9e548667b7d8cef7d98caddf1d25e76c
Dina Migunova <migunova.dina@gmail.com>
2023-03-16 10:44:25 +0100

    Add Person toString()

--- src/hw_40/Mainhw_40.java
+++ src/hw_40/Mainhw_40.java
@@ -1,4 +1,8 @@
 package hw_40;
 
 public class Mainhw_40 {
+    public static void main(String[] args) {
+        Person human = new Person("Dina");
+        System.out.println(human);
+    }
 }
--- src/hw_40/Person.java
+++ src/hw_40/Person.java
@@ -14,4 +14,9 @@ public class Person {
     public void setName(String name) {
         this.name = name;
     }
+
+    @Override
+    public String toString() {
+        return "Человек по имени " + name;
+    }
 }
